d8e70f454a1f7ccaf07dd9d073196f3ba7b21007
Hijazi <ctcrdneh@opm.gov>
2020-02-10 23:39:31 -0500

    added main class into package com.essahijazi

---
+++ src/com/essahijazi/Main.java
@@ -0,0 +1,9 @@
+package com.essahijazi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This class' source code file structure is src -> com -> essahijazi -> Main.java");
+	}
+
+}
